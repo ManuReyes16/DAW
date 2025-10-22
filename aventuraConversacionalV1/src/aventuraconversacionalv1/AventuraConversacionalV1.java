879c98f3e9d22389ea15cc5252a9936a5f3d879f
ManuReyes16 <manureyesnavarro11@gmail.com>
2025-10-22 11:28:25 +0200

    aventura1

---
+++ aventuraConversacionalV1/src/aventuraconversacionalv1/AventuraConversacionalV1.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aventuraconversacionalv1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author EAG
+ */
+public class AventuraConversacionalV1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Bienvenido a este juego");
+        Scanner sc = new Scanner (System.in);
+        
+       
+        String , izqda;
+        
+                                
+        System.out.println("Elige un camino");
+        String cam1 = sc.nextLine();
+        
+       switch (decha){
+           case 1: System.out.println("");
+       }
+               
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
